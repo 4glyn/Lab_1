a92bd5031ff90b76bd3cf1c0fdb3b7aa4dc6d67d
Angeline <angeline@Angelines-MacBook-Air.local>
2026-08-29 22:26:47 +0800

    Week 1: verified C++ environemnt

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // enables std::cout for output
+using namespace std;
+
+int main (){
+    // A simple mesage to confirm compilation succeeded
+    cout << "Environemnt verified. Ready for Data Structures." << endl;
+    return 0; // 0 signals successful program exit
+}
